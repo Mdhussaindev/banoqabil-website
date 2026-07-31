75ac4011d01bc07c6b566e8e4a856d9b757e8df8
Mdhussaindev <mdhussain.dev00@gmail.com>
2026-07-31 20:21:11 +0500

    update

--- app.js
+++ app.js
@@ -6,3 +6,33 @@ menuBtn.addEventListener("click", () => {
     navLinks.classList.toggle("active");
 
 });
+
+const counters = document.querySelectorAll(".stat-card h2");
+
+counters.forEach(counter => {
+
+    const updateCounter = () => {
+
+        const target = parseInt(counter.innerText);
+
+        const current = +counter.innerText.replace("+","");
+
+        const increment = target / 80;
+
+        if(current < target){
+
+            counter.innerText = `${Math.ceil(current + increment)}+`;
+
+            setTimeout(updateCounter,20);
+
+        }else{
+
+            counter.innerText = target + "+";
+
+        }
+
+    };
+
+    updateCounter();
+
+});
